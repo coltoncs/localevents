b1f6906519317e8f9325e65f6178201c1b51a476
Colton Sweeney <colton.sweeney@gmail.com>
2026-01-04 11:04:02 -0500

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,9 +1,8 @@
 import { handleRequest } from '@vercel/react-router/entry.server'
 import { RouterContextProvider } from 'react-router'
 
-// this is a workaround to provide RouterContextProvider 
-// as loadContext for Vercel's React Router wrapper. Otherwise our
-// middleware setup would not work correctly. i.e. Clerks middleware
+// We can remove this in the future, this is a workaround
+// for Vercel's current lack of support for middleware
 export function getLoadContext() {
   // Return an instance of RouterContextProvider when middleware is enabled
   return new RouterContextProvider()
